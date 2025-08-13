988a2032b50a050fbe9a55a4895c92cad6a7618d
Sanju-1625 <sanjuartkumar@gmail.com>
2025-08-13 11:44:33 +0530

    Update sanju10.py "planing for future"

---
+++ sanju10.py
@@ -0,0 +1,6 @@
+Teju sanju are in relationship
+sanju are trying to get the devops job as soon as possible
+i need to plan for a 25lac loan to build new house
+i need to re-constructed to our house.
+teju parents will accept if i shettled a good job
+after that teju and me are getting marriage 
